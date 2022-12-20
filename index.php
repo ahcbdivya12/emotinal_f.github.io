659ef3519e33e2cbb84dfd08307fbd1e1d08fbc8
ahcbdivya12 <121059036+ahcbdivya12@users.noreply.github.com>
2022-12-20 23:21:32 +0530

    my

---
+++ index.php
@@ -0,0 +1,163 @@
+
+<?php
+include 'navigation.php';
+
+?><!DOCTYPE html>	
+<html>
+<head>
+	<title></title>
+<link rel="stylesheet" type="text/css" href="css/main_home.css">
+
+<link rel="stylesheet" type="text/css" href="css/emoji_css.css">
+
+<link rel="stylesheet" type="text/css" href="css/main2.css">
+<link rel="stylesheet" href="css/footer.css">
+<style>
+footer {
+display: flex;
+  position: absolute;
+  bottom: 0;
+  border-top: 1px solid #EEEEEE;
+  padding: 20px 0;
+  width: 100%;
+}
+.logo img{
+  float: left;
+  width: 150px;
+  height: auto;
+  margin-left: 20px;
+  margin-top: -20px;
+}
+
+header nav a:hover {
+    border-bottom: 1px solid #000;
+}
+  </style>
+</head>
+<body style="">
+<?=template_h('Index')?>
+  <main style="background-color: #EEE;" >
+  <div class="title">
+		<h1 style="color: #000;">Emotional Fund</h1>
+	</div>
+	<div class="button">
+		<a style="border: 1px solid #000;color: #fff;" href="#" class="btn"><b>Books</b></a>
+		<a style="border: 1px solid #000;color: #fff;" href="#" class="btn"><b>Musics</b></a>
+	</div>
+	 <div class="container"> 
+       <!-- Content of the body-->
+        <div class="row" >
+            <div class="column"style="margin-top: -50px; margin-left: -10px;">
+                <div class="content">
+                    <a href="jounrul\happy.html"><img src="images\happy_emoji-removebg-preview.png"
+                        alt="" style="width:60%">
+                    <h3>
+                        <a href="D:\xampp\htdocs\emo\jounrul\happy.html">Select Happy Jounrul</a>
+                    </h3>
+          </a>          <br>
+                     <p>
+                      <b><i>"You only live once, but if you do it right, once is enough."</b></i><br><b style="text-decoration: none;
+        						color: #5673C8;">-- Mae West</b>
+					 </p>
+
+               </div>
+            </div>
+             
+            <div class="column"style="margin-top: -50px;">
+                <div class="content">
+                  <a href="jounrul\angry.html">  <img src=
+					"images\angry-removebg-preview.png"
+                        alt="" style="width:47%;padding:5px;">
+                  
+					<h3>
+                        <a href="D:\xampp\htdocs\emo\jounrul\angry.html">Select Angry Jounrul</a>
+                    </h3><br>
+					<p>
+                       <b><i> "Bitterness is like cancer. It eats upon the host. But anger is like fire. It burns it all clean."</i></b>
+					<b style="text-decoration: none;color: #5673C8;"><br>-- Maya Angelou</b> </p>
+       				</a> 
+       		    </div>
+            </div>
+             
+            <div class="column"style="margin-top: -50px;">
+                <div class="content">
+                    <a href="jounrul\gultiy.html"><img src=
+"images\gultiy-removebg-preview.png"
+                        alt="" style="width:67%">
+                    <h3>
+                        <a href="D:\xampp\htdocs\emo\jounrul\gultiy.html">select Gultiy Jounrul</a>
+                    </h3><br>
+<p>                      <b><i>" The most beautiful words in the English language are 'not guilty'".</i></b>
+<b style="text-decoration: none;color: #5673C8;"><br>-- Maxim Gorky</b> 
+                       </p>
+ </a>
+                </div>
+            </div>
+             
+            <div class="column"style="margin-top: -50px;">
+                <div class="content">
+                    <a href="jounrul\sad.html"><img src=
+"images\sad_1.jpg"
+                        alt="" style="width:55%;">
+     				<h3>
+                        <a href="D:\xampp\htdocs\emo\jounrul\sad.html">select Sad Jounrul</a>
+                    </h3>
+<br>	                     <p> <b><i>"It's amazing how someone can break your heart and you can still love them with all the little pieces."</i></b>
+<b style="text-decoration: none;color: #5673C8;"><br>-- Ella Harper </b> 
+                        </p>
+ </a>
+ </div>
+            </div>
+        </div>
+    </div>  
+
+  
+<div  class="clearfix" >
+   <a href="jounrul.html"><img class="img2" src="images\all_emostil.jpg" alt="Pineapple" width="170" height="170">  
+  </a><p style="margin-top:-350px;float:left;line-height: 40px;text-align:center;
+font-weight:bold; font-size:20px;margin-right:500px;color: #5673C8;height: 500px;">
+  “Emotions, in my experience, aren't covered by single words. I don't believe in "sadness," "joy," or "regret." Maybe the best proof that the language is patriarchal is that it oversimplifies feeling. I'd like to have at my disposal complicated hybrid emotions, Germanic train-car constructions like, say, "the happiness that attends disaster." Or: "the disappointment of sleeping with one's fantasy." I'd like to show how "intimations of mortality brought on by aging family members" connects with "the hatred of mirrors that begins in middle age." I'd like to have a word for "the sadness inspired by failing restaurants" as well as for "the excitement of getting a room with a minibar." I've never had the right words to describe my life, and now that I've entered my story, I need them more than ever. ”</p>
+</div>
+
+<div>
+   <a href="book/index.php"><img class="img3" src="images/book_baground.jpg" alt="Pineapple" width="170" height="170">
+   </a> <p style="margin-top:-220px;line-height: 40px;text-align:center;
+font-weight:bold; font-size:20px;margin-left:300px;color: #5673C8;">
+  “Books can also provoke emotions. And emotions sometimes are even more troublesome than ideas. Emotions have led people to do all sorts of things they later regret-like, oh, throwing a book at someone else.” <br>“Life is a comedy to those who think, a tragedy to those who feel.” 
+</p>
+</div>
+
+<div>
+   <a href="Music/index.php"><img class="img4" src="music\img\bg_music.jpg" alt="" width="170" height="170">
+</a> <h1 style="float: left; margin-top: 150px; margin-left: 500px;color: #5673C8">Best Music</h1>   <p style="color: #5673C8;font-weight:bold; font-size:17px;text-align:center;margin-top:120px;float:left; ">
+ “Music expresses feeling and thought, without language; it was below and before speech, and it is above and beyond all words.”
+</p>
+
+</div>
+<div>
+   <a href="theripist/index.php"><img class="img3" src="theripist\img\Top-Psychologists-in-India.png" alt="Pineapple" width="170" height="170">
+ </a>
+ <h1 style="color: #5673C8;text-align:center;margin-top: -120px; margin-left:100px;">Top Psychologists in India</h1>
+    <p style="color: #5673C8;text-align:center;margin-top: -150px;align-content: center; margin-left:100px;">
+      
+<b >Consult Best Psychologist & Counsellor in India</b><br>
+Best Psychotherapists, Counsellors, Mental Health Therapists in India.
+<br>Connect with top psychiatrist online. Get Psychiatric treatment online.<br>
+<b>Certified & Experienced</b><br>
+Get the best online Therapy & Counseling Experience from the Verified Professionals.
+<br>
+<b>Best Psychiatrists in India</b><br>
+
+
+We assign the best psychologist and counsellor as per your case. We Choose the best for you.
+ </p>
+</div>
+<div class="content-wrapper" style="margin-top: 200px;">
+  <?php
+  $year = date('Y');
+  ?>
+      <p>&copy; <?php echo $year?>, Emotinal featcure</p>
+</div>
+</main>
+</body>
+</html>
